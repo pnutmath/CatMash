c0f4f15d7f2ecab7f2f0deea15ee2be0df296c57
VINODEA Richard <richard.vinodeaa@gmail.com>
2019-11-08 12:17:09 +0100

    add services

---
+++ CatMashProject/src/app/model/cat.ts
@@ -0,0 +1,9 @@
+export interface cat {
+    url: string;
+    id: string;
+    rating?: number;
+}
+
+export interface CatList {
+    images: Cat[];
+}
--- CatMashProject/src/app/services/cat.service.ts
+++ CatMashProject/src/app/services/cat.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { cat, CatList } from '../model/cat';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatService {
+  catVote: cat[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCat(): Observable<CatList> {
+    return this.http.get<CatList>('http//localhost:8080/cats').pipe(
+      tap((catList: CatList) => {
+        if (this.catVote.length === 0) {
+          this.catVote = catList.images;
+        }
+      })
+    )
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
